5b01f1214cb0bac938741e3a66db40f79ccaeb2f
kirin2461-OSS <133155360+kirin2461-OSS@users.noreply.github.com>
2026-07-30 14:35:40 +0300

    trainer
    market + add

--- daemon_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import sys, os
-sys.path.insert(0, '/home/user/.local/lib/python3.7/site-packages')
-os.chdir('/home/user/trainer-marketplace')
-os.environ['PYTHONPATH'] = '/home/user/.local/lib/python3.7/site-packages'
-
-import uvicorn
-uvicorn.run('app.main:app', host='127.0.0.1', port=8000, reload=False, workers=1, log_level='warning')
